c06606962231820056b1061d99a3be7d61a73a48
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-09-22 19:30:47 +0000

    Attempt to detect general nmap scan

---
+++ alarm/README.md
@@ -0,0 +1,25 @@
+## Incident Alarm
+1. Identify what aspects of the work have been correctly implemented and what have not.
+
+    > TODO
+
+2. Identify anyone with whom you have collaborated or discussed the assignment.
+
+    > Obaid Farooqui, Melissa Blotner
+
+3. Approximate number of hours spent on the assignment.
+
+    > 4 hours
+
+
+
+### Questions
+1. Are the heuristics used in this assignment to determine incidents
+    "even that good"?
+
+    > Many of the heuristics used will result in false positives; for example, a NULL scan may be detected even if it is not malicious and some strings of numbers will fit the criteria we use to determine if a number is a credit card number but it may not actually be a credit card number. Furthermore, we check for some of the incidents by checking for keywords in the payload. If an attacker did not include the keyword, we may not detect the incident. So, though th heuristics will catch many potential incidents, there may be some false positives and false negatives.
+
+2. If you have spare time in the future, what would you add to the program or do differently with regards to detecting incidents?
+
+    > TODO
+
--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -8,10 +8,10 @@ def print_alert(incident_num, incident, source_ip, protocol, payload)
 end
 
 def parse_payload(payload)
-    puts "PAYLOAD:"
+    #puts "PAYLOAD:"
     if !payload.empty?
-        puts Base64.encode64(payload)
-        puts payload.unpack("H*")
+    #    puts Base64.encode64(payload)
+    #    puts payload.unpack("H*")
     end
 end
 
@@ -25,7 +25,7 @@ def parse_log_line(line)
         protocol = split_request.length > 2 ? split_request[2] : ""
         split_protocol = protocol.split("/")
         protocol = split_protocol.length > 0 ? split_protocol[0] : ""
-        payload = split_request.length > 1 ? split_request[0]  + " " + split_request[1] : ""
+        payload = split_request.length > 1 ? split_response[0,2].join(" ") : " "
     end
     return source_ip, request, protocol, payload
 end
@@ -51,14 +51,17 @@ if (opt_index != nil)
                     elsif downcase_line.include?("nmap")
                         print_alert(index, "Nmap scan", source_ip, protocol, payload)
                         index += 1
+                    elsif downcase_line.include?("nikto")
+                        # TODO: check that this actually detects nikto
+                        print_alert(index, "Nikto scan", source_ip, protocol, payload)
                     elsif downcase_line.include?("masscan")
                         print_alert(index, "Masscan", source_ip, protocol, payload)
                         index += 1
                     elsif no_space_line.include?("(){:;};")
-                        print_alert(index, "Potential Shellshock scan", source_ip, protocol, payload)
+                        print_alert(index, "Shellshock scan", source_ip, protocol, payload)
                         index += 1
                     elsif line =~ /([\\\\][x][a-zA-z\d]{2})+/
-                        print_alert(index, "Potential shellcode", source_ip, protocol, payload)
+                        print_alert(index, "Shellcode", source_ip, protocol, payload)
                     end
                 end
             end
@@ -72,20 +75,23 @@ else
     stream.stream.each do |packet|
         pkt = PacketFu::Packet.parse(packet)
         parse_payload(pkt.payload)
-        if pkt.class == PacketFu::TCPPacket
-            puts pkt.tcp_flags
+        if pkt.payload.match(/\x4E\x6D\x61\x70/)
+            print_alert(index, "Nmap scan", pkt.ip_saddr, pkt.proto().last, pkt.payload.to_s)
+            index += 1
+        elsif pkt.class == PacketFu::TCPPacket
+            #puts pkt.tcp_flags
             flags = pkt.tcp_flags
             if flags.syn == 0 and flags.rst == 0 and flags.ack == 0
                 if flags.select{ |f| f == 1}.empty?
-                    print_alert(index, "NULL scan", pkt.ip_src, pkt.proto().last, pkt.payload.to_s)
+                    print_alert(index, "NULL scan", pkt.ip_saddr, pkt.proto().last, pkt.payload.to_s)
                     index += 1
                 end
                 if flags.fin == 1 and flags.urg == 0 and flags.psh == 0
-                    print_alert(index, "FIN scan", pkt.ip_src, pkt.proto().last, pkt.payload.to_s)
+                    print_alert(index, "FIN scan", pkt.ip_saddr, pkt.proto().last, pkt.payload.to_s)
                     index += 1
                 end
                 if flags.fin == 1 and flags.psh == 1 and flags.urg == 1
-                    print_alert(index, "XMAS scan", pkt.ip_src, pkt.proto().last, pkt.payload.to_s)
+                    print_alert(index, "XMAS scan", pkt.ip_saddr, pkt.proto().last, pkt.payload.to_s)
                     index += 1
                 end
             end
