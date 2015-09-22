be8c009ee8694434982bd06fd2ed52324b40f5d4
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-09-22 23:43:41 +0000

    Fix a few typos

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -25,14 +25,14 @@ def parse_log_line(line)
         protocol = split_request.length > 2 ? split_request[2] : ""
         split_protocol = protocol.split("/")
         protocol = split_protocol.length > 0 ? split_protocol[0] : ""
-        payload = split_request.length > 1 ? split_response[0,2].join(" ") : " "
+        payload = split_request.join(" ")
     end
     return source_ip, request, protocol, payload
 end
 
 args = ARGV
 opt_index = args.index("-r")
-index = 0
+index = 1
 
 if (opt_index != nil)
     if args.length < opt_index + 2
@@ -48,20 +48,26 @@ if (opt_index != nil)
                     if downcase_line.include?("phpmyadmin")
                         print_alert(index, "Someone looking for phpMyAdmin stuff", source_ip, protocol, payload)
                         index += 1
-                    elsif downcase_line.include?("nmap")
+                    end
+                    if downcase_line.include?("nmap")
                         print_alert(index, "Nmap scan", source_ip, protocol, payload)
                         index += 1
-                    elsif downcase_line.include?("nikto")
+                    end
+                    if downcase_line.include?("nikto")
                         # TODO: check that this actually detects nikto
                         print_alert(index, "Nikto scan", source_ip, protocol, payload)
-                    elsif downcase_line.include?("masscan")
+                    end
+                    if downcase_line.include?("masscan")
                         print_alert(index, "Masscan", source_ip, protocol, payload)
                         index += 1
-                    elsif no_space_line.include?("(){:;};")
+                    end
+                    if no_space_line.include?("(){:;};")
                         print_alert(index, "Shellshock scan", source_ip, protocol, payload)
                         index += 1
-                    elsif line =~ /([\\\\][x][a-zA-z\d]{2})+/
+                    end
+                    if line =~ /([\\\\][x][a-zA-z\d]{2})+/
                         print_alert(index, "Shellcode", source_ip, protocol, payload)
+                        index += 1
                     end
                 end
             end
